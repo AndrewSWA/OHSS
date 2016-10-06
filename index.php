a1a0637d6f34c558364c2eb7190eecfd4e32db2b
AndrewSILS <amcdonald@sasklibraries.ca>
2016-10-06 14:03:23 -0400

    added a dummy form place holder and reset functionality

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php 
 
-print('This is the beginning of OHSS');
+require_once("scan_form.php");
+
+//TODO: if form posted handle request.
+if(!isset($_SESSION)){
+	session_start();
+}
+
+if(!isset($_SESSION['TEST'])) {
+	$_SESSION['TEST'] = 1;
+}
+else {
+	$_SESSION['TEST']++;
+}
+
+print($_SESSION['TEST']);
+print print_scan_form();
+// print_scan_order_details();
---
+++ scan_form.php
@@ -0,0 +1,6 @@
+<?php
+
+function print_scan_form() {
+	//TODO: generate form here.
+	return "<h4>form goes here<h4>";
+}
