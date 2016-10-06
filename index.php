b90914671bb866cef4d94b6aeac3fd7406cd4d7b
AndrewSILS <amcdonald@sasklibraries.ca>
2016-10-06 15:38:31 -0400

    handled the form post and processed the barcode

--- index.php
+++ index.php
@@ -3,12 +3,15 @@
 require_once("scan_form.php");
 require_once("scanned_items.php");
 require_once("scanned_results.php");
+require_once("process_barcode.php");
 
-//TODO: if form posted handle request.
 if(!isset($_SESSION)){
 	session_start();
 }
 
+if(isset($_POST['barcode'])) {
+	add_item(processBarcode($_POST['barcode']));
+}
 print print_scan_form();
 print render_scanned_results();
 // print_scan_order_details();
--- process_barcode.php
+++ process_barcode.php
@@ -2,7 +2,7 @@
 define('BARCODE_ENDPOINT', 'http://library2.udayton.edu/ray/ohss/barcode_search.php?barcode=%s');
 
 function processBarcode($barcode) {
-	if (preg_match('/^[A-Z][0-9]+$/', $barcode)) {
+	if (preg_match('/^[A-Z][0-9]+$/i', $barcode)) {
 		$result = file_get_contents(sprintf(BARCODE_ENDPOINT, $barcode));
 
 		if ($result) {
