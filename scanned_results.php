a5ece705884c15467355ba831a9e6eed0983468a
John Kloor <john@kloor.org>
2016-10-06 19:42:38 +0000

    Add table of results marking books out-of-order.

--- scanned_results.php
+++ scanned_results.php
@@ -1,12 +1,62 @@
 <?php
 
 function render_scanned_results() {
-	$scanned = print_r($_SESSION['scanned'], true);
-	$sorted =  print_r($_SESSION['sorted'], true);
-	return <<<OUTPUT
-	<h3>SCANNED ORDER</h3>
-	<pre>$scanned</pre>
-	<h3>SORT ORDER</h3>
-	<pre>$sorted</pre>
-OUTPUT;
+	$prev = reset($_SESSION['scanned']);
+	$curr = next($_SESSION['scanned']);
+	
+	if ($curr && $prev['call_number_norm'] >= $curr['call_number_norm']) {
+		$prev['error'] = true;
+	}
+
+	print('<table>');
+	print('<tr>');
+
+	foreach (array_keys($prev) as $key) {
+		if ($key === 'error') {
+			continue;
+		}
+
+		print('<th>');
+		print(ucwords(str_replace('_', ' ', $key)));
+		print('</th>');
+	}
+
+	print('</tr>');
+
+	render_scanned_individual_result($prev);
+
+	while ($curr) {
+		$next = next($_SESSION['scanned']);
+
+		if ($curr['call_number_norm'] <= $prev['call_number_norm']) {
+			$curr['error'] = true;
+		} elseif ($next && $curr['call_number_norm'] >= $next['call_number_norm']) {
+			$curr['error'] = true;
+		}
+
+		render_scanned_individual_result($curr);
+
+		$prev = $curr;
+		$curr = $next;
+	}
+
+	print('</table>');
+}
+
+function render_scanned_individual_result($curr) {
+	if (empty($curr['error'])) {
+		print('<tr>');
+	} else {
+		print('<tr style="background:red">');
+	}
+
+	foreach ($curr as $key => $value) {
+		if ($key === 'error') {
+			continue;
+		}
+
+		print('<td>' . $value . '</td>');
+	}
+
+	print('</tr>');
 }
