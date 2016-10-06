8165efc8989832d2b8ccc1e4596a6579034de753
AndrewSILS <amcdonald@sasklibraries.ca>
2016-10-06 15:50:06 -0400

    added sample barcodes

--- index.php
+++ index.php
@@ -12,7 +12,9 @@ if(!isset($_SESSION)){
 if(isset($_POST['barcode'])) {
 	add_item(processBarcode($_POST['barcode']));
 }
+$output = file_get_contents('sample_barcodes.txt');
 print "<a href='reset.php'>Click to restart</a><br><br>";
+print "<pre>$output</pre>";
 print print_scan_form();
 print render_scanned_results();
 // print_scan_order_details();
