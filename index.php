8936b888a676aa00a55301dcf091dcfce1a90ffa
AndrewSILS <amcdonald@sasklibraries.ca>
2016-10-06 15:29:42 -0400

    modified index.php to include the new files

--- index.php
+++ index.php
@@ -1,19 +1,14 @@
 <?php 
 
 require_once("scan_form.php");
+require_once("scanned_items.php");
+require_once("scanned_results.php");
 
 //TODO: if form posted handle request.
 if(!isset($_SESSION)){
 	session_start();
 }
 
-if(!isset($_SESSION['TEST'])) {
-	$_SESSION['TEST'] = 1;
-}
-else {
-	$_SESSION['TEST']++;
-}
-
-print($_SESSION['TEST']);
 print print_scan_form();
+print render_scanned_results();
 // print_scan_order_details();
