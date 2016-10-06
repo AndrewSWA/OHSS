44df0682b368c7ce9562536f176fefbcf0f068dd
John Kloor <john@kloor.org>
2016-10-06 18:23:01 +0000

    Add function body for processBarcode.

--- process_barcode.php
+++ process_barcode.php
@@ -1,5 +1,14 @@
 <?php
+define('BARCODE_ENDPOINT', 'http://library2.udayton.edu/ray/ohss/barcode_search.php?barcode=%s');
 
 function processBarcode($barcode) {
+	if (preg_match('/^[A-Z][0-9]+$/', $barcode)) {
+		$result = file_get_contents(sprintf(BARCODE_ENDPOINT, $barcode));
 
+		if ($result) {
+			return json_decode($result, true);
+		}
+	}
+
+	return false;
 }
